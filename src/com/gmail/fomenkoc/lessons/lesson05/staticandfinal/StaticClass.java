75d24c81cadc7bcb8182735a0f7f7c7b6ae48e89
fomenkoc <fomenkoc@gmail.com>
2021-09-01 17:22:34 +0300

    lesson 5 start

---
+++ src/com/gmail/fomenkoc/lessons/lesson05/staticandfinal/StaticClass.java
@@ -0,0 +1,16 @@
+package com.gmail.fomenkoc.lessons.lesson05.staticandfinal;
+
+public  class  StaticClass {
+
+	public final static String MY_NAME = "Vitalii";
+	String myName = "Andrew";
+
+	static void sayHello() {
+		System.out.println("Hello static");
+	}
+	
+	  void sayHi() {
+			System.out.println("Hi static");
+		}
+
+}
