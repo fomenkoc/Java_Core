efd337fc0904d37f5e89c9fb9ac32b08d2c602c1
fomenkoc <fomenkoc@gmail.com>
2021-09-10 10:00:39 +0300

    test

--- src/com/gmail/fomenkoc/homeworks/homework09/HW08App.java
+++ src/com/gmail/fomenkoc/homeworks/homework09/HW08App.java
@@ -5,6 +5,7 @@ import com.gmail.fomenkoc.homeworks.homework09.classes.Menu;
 public class HW08App {
 
     public static void main(String[] args) {
+	System.out.println("Hello");
 	Menu.start();
 
     }
