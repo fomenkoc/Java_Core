185360e2d5953a0ce4e23d7d431a14867f858834
fomenkoc <fomenkoc@gmail.com>
2021-09-17 10:57:11 +0300

    hw 10

--- src/com/gmail/fomenkoc/homeworks/homework10/HW10App.java
+++ src/com/gmail/fomenkoc/homeworks/homework10/HW10App.java
@@ -11,22 +11,26 @@ public class HW10App {
 	IsFiveLettersPalindrome.check("Комок");
 	IsFiveLettersPalindrome.check("qwert");
 	IsFiveLettersPalindrome.check("Civic");
-	
+
 	System.out.println();
-	
-	
-	System.out.println( Vowels.replace("тестова стрічка"));
-	System.out.println( Vowels.replace("test string"));
-	System.out.println( Vowels.replace("TEST STRING"));
-	
+
+	System.out.println(Vowels.replace("тестова стрічка"));
+	System.out.println(Vowels.replace("test string"));
+	System.out.println(Vowels.replace("TEST STRING"));
+
 	System.out.println();
-	
-	
+
 	String string = "A long time ago in a galaxy far, far away....";
-	
-	Sentence.wordsCount(string);
-	
-	
+	String string2 = "Колись давно у далекій - далекій галактиці...";
+
+	Sentence.showWordsCount(string);
+	Sentence.showWordsCount(string2);
+
+	System.out.println();
+
+	Sentence.showTopWord(string);
+	Sentence.showTopWord(string2);
+
     }
 
 }
--- src/com/gmail/fomenkoc/homeworks/homework10/classes/Sentence.java
+++ src/com/gmail/fomenkoc/homeworks/homework10/classes/Sentence.java
@@ -1,14 +1,54 @@
 package com.gmail.fomenkoc.homeworks.homework10.classes;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Sentence {
 
-    public static void wordsCount(String string) {
-	String[] strings = string.split(" ", 0);
+    public static void showWordsCount(String string) {
+	String[] strings = getSplitWords(string);
 	int wordsCount = 0;
+
+	for (String str : strings) {
+	    if (str.length() > 2)
+		wordsCount++;
+	}
+
+	System.out.println("\"" + string + "\"" + " - contains "
+		+ wordsCount + " words");
+    }
+
+    public static void showTopWord(String string) {
+	String[] strings = getSplitWords(string);
+	int[] repeat = new int[strings.length];
+	int count;
+	int topWordIndex;
+	for (int i = 0; i < repeat.length; i++) {
+	    count = 0;
+	    if (strings[i].length() > 2) {
+		for (int j = i; j < repeat.length; j++) {
+		    if (strings[i].equalsIgnoreCase(strings[j])) {
+			count++;
+		    }
+		}
+	    }
+	    repeat[i] = count;
+	}
+
+	count = 0;
+	topWordIndex = 0;
+
+	for (int i = 0; i < repeat.length; i++) {
+	    if (repeat[i] > count) {
+		count = repeat[i];
+		topWordIndex = i;
+	    }
+	}
+
+	System.out.println(
+		"\"" + strings[topWordIndex] + "\" is a top word.");
+
+    }
+
+    static String[] getSplitWords(String string) {
+	String[] strings = string.split(" ", 0);
 
 	for (int i = 0; i < strings.length; i++) {
 	    if (strings[i].contains("!")) {
@@ -41,15 +81,12 @@ public class Sentence {
 	    if (strings[i].contains("\'")) {
 		strings[i] = strings[i].replace("\'", "");
 	    }
+	    if (strings[i].contains("-'")) {
+		strings[i] = strings[i].replace("-'", "");
+	    }
 	}
 
-	for (String str : strings) {
-	    if (str.length() > 2)
-		wordsCount++;
-	}
-
-	System.out.println("\"" + string + "\"" + " - contains "
-		+ wordsCount + " words");
+	return strings;
     }
 
 }
