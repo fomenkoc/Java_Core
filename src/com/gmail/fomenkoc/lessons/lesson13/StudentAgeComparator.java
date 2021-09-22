dbd6a9b6248e19ac6c6c0fcaff4fe087c67fa30f
fomenkoc <fomenkoc@gmail.com>
2021-09-22 16:41:53 +0300

    lesson 13

---
+++ src/com/gmail/fomenkoc/lessons/lesson13/StudentAgeComparator.java
@@ -0,0 +1,14 @@
+package com.gmail.fomenkoc.lessons.lesson13;
+
+
+import java.util.Comparator;
+
+public class StudentAgeComparator  implements Comparator<Student>{
+
+	@Override
+	public int compare(Student o1, Student o2) {
+	
+		return o1.getAge()>o2.getAge()?1:-1;
+	}
+
+}
