5604c513453f566224b1ae9481d43e7e36c90994
fomenkoc <fomenkoc@gmail.com>
2021-10-05 17:33:13 +0300

    lesson 17

---
+++ src/com/gmail/fomenkoc/lessons/lesson17/PersonFactory.java
@@ -0,0 +1,96 @@
+package com.gmail.fomenkoc.lessons.lesson17;
+
+public class PersonFactory {
+
+//	Inner class
+	private class SportsMan implements Personable {
+		@Override
+		public void eat() {
+			System.out.println("Sportsman is eating");
+
+		}
+
+		@Override
+		public void move() {
+			System.out.println("Sportsman is moving");
+		}
+
+		@Override
+		public void sleep() {
+			System.out.println("Sportman is sleeping");
+		}
+	}
+
+	public SportsMan createSportsMan() {
+		return new SportsMan();
+	}
+
+	// Local class
+	public Personable createHouseWifeLocal() {
+		class HouseWife implements Personable {
+
+			@Override
+			public void eat() {
+				System.out.println("HouseWife is eating");
+			}
+
+			@Override
+			public void move() {
+				System.out.println("HouseWife is moving");
+			}
+
+			@Override
+			public void sleep() {
+				System.out.println("HouseWife is sleeping");
+			}
+
+		}
+
+		return new HouseWife();
+	}
+
+	// Anonymous class
+	public Personable createAnonymous() {
+		return new Personable() {
+
+			@Override
+			public void eat() {
+				System.out.println("Anonymous is eating");
+			}
+
+			@Override
+			public void move() {
+				System.out.println("Anonymous is moving");
+			}
+
+			@Override
+			public void sleep() {
+				System.out.println("Anonymous is sleeping");
+			}
+		};
+	}
+
+	//static neasted
+	private static class GolfPlayer implements Personable {
+
+		@Override
+		public void eat() {
+			System.out.println("Golf player is eating");
+		}
+
+		@Override
+		public void move() {
+			System.out.println("Golf Player is moving");
+		}
+
+		@Override
+		public void sleep() {
+			System.out.println("Golf player is sleeping");
+		}
+	}
+	
+	public static GolfPlayer createGolfPlayer() {
+		return new GolfPlayer();
+	}
+	
+}
---
+++ src/com/gmail/fomenkoc/lessons/lesson17/Personable.java
@@ -0,0 +1,7 @@
+package com.gmail.fomenkoc.lessons.lesson17;
+
+public interface Personable {
+	public void eat();
+	public void move();
+	public void sleep();
+}
